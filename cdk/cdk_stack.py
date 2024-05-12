84ba9d4f37802eb1fd681c12d0e4ebe370d3ee73
Nick Hunt <nick@ps.tech>
2024-05-13 00:01:36 +0100

    working cdk with two s3 buckets

---
+++ cdk/cdk_stack.py
@@ -0,0 +1,35 @@
+from constructs import Construct
+from aws_cdk import (
+    Duration,
+    Stack,
+    aws_iam as iam,
+    aws_sqs as sqs,
+    aws_sns as sns,
+    aws_sns_subscriptions as subs,
+    aws_s3 as s3,
+)
+
+
+class CdkStack(Stack):
+
+    def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
+        super().__init__(scope, construct_id, **kwargs)
+
+        bucket = s3.Bucket(
+            self, "MyFirstBucket",
+            versioned=True,
+        )
+        bucket2 = s3.Bucket(
+            self, "MySecondBucket",
+            versioned=True,
+        )
+        # queue = sqs.Queue(
+        #     self, "CdkQueue",
+        #     visibility_timeout=Duration.seconds(300),
+        # )
+
+        # topic = sns.Topic(
+        #     self, "CdkTopic"
+        # )
+
+        # topic.add_subscription(subs.SqsSubscription(queue))
